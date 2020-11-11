1d0eca55b789312f6dcbf89fc2431290a37b8c62
JohnathanSanchez <74288604+JohnathanSanchez@users.noreply.github.com>
2020-11-11 09:32:30 -0500

    AutoBuy.lua 1.0
    Working AutoBuy Lua for Nixware.cc CSGO 
    
    I still need to add a few primary weapons which will be added soon - report bugs to kawsta#8338

--- AutoBuy.lua
+++ AutoBuy.lua
@@ -1,7 +1,7 @@
 ui.add_checkbox("AutoBuy", "b_autobuy", false)
 
-ui.add_combo("Primary", "b_primary", { "NONE", "AWP", "AUTO", "SCOUT", "AK47/M4A4", "AUG/SG553", "FAMAS/GALIL", "MAG7/SAWED OFF", "NOVA" } 0)
-ui.add_combo("Secondary", "b_secondary", { "NONE", "DEAGLE/R8", "DUALIES", "P250", "FIVESEVEN/TEC9/CZ75" }  0)
+ui.add_combo("Primary", "b_primary", { "NONE", "AWP", "AUTO", "SCOUT", "AK47/M4A4", "AUG/SG553", "FAMAS/GALIL", "MAG7/SAWED OFF", "NOVA"}, 0)
+ui.add_combo("Secondary", "b_secondary", { "NONE", "DEAGLE/R8", "DUALIES", "P250", "FIVESEVEN/TEC9/CZ75"},  0)
 
 
 ui.add_checkbox("FLASHBANG", "buy_flash", false)
@@ -24,9 +24,89 @@ local function buy_bot(event)
     end
     
     if ui.get_int("b_primary") == 1 then
-        engine.execute_client_cmd("buy awp")
+      engine.execute_client_cmd("buy awp")
     end
-                       
+
+    if ui.get_int("b_primary") == 2 then
+      engine.execute_client_cmd("buy scar20; buy g3sg1")              
+    end
+    
+    if ui.get_int("b_primary") == 3 then
+      engine.execute_client_cmd("buy ssg08")  
+    end
+    
+    if ui.get_int("b_primary") == 4 then
+      engine.execute_client_cmd("buy ak47; buy m4a1")
+    end
+
+    if ui.get_int("b_primary") == 5 then
+        engine.execute_client_cmd("buy sg556; buy aug")
+    end
+    
+    if ui.get_int("b_primary") == 6 then
+        engine.execute_client_cmd("buy galilar; buy famas")
+    end
+
+    if ui.get_int("b_primary") == 7 then
+        engine.execute_client_cmd("buy sawedoff; buy mag7")
+    end
+    
+    if ui.get_int("b_primary") == 8 then
+        engine.execute_client_cmd("buy nova")
+    end
+
+    if ui.get_int("b_secondary") == 0 then
+        engine.execute_client_cmd(" ")
+    end
+
+    if ui.get_int("b_secondary") == 1 then
+        engine.execute_client_cmd("buy revolver; buy deagle")
+    end
+    
+    if ui.get_int("b_secondary") == 2 then
+        engine.execute_client_cmd("buy elite")
+    end
+
+    if ui.get_int("b_secondary") == 3 then
+        engine.execute_client_cmd("buy p250")
+    end
+
+    if ui.get_int("b_secondary") == 4 then
+        engine.execute_client_cmd("buy tec9; buy fiveseven")
+    end
+
+    if ui.get_bool("buy_flash") then
+        engine.execute_client_cmd("buy flashbang")
+    end
+
+    if ui.get_bool("buy_decoy") then
+        engine.execute_client_cmd("buy decoy")
+    end
+
+    if ui.get_bool("buy_he") then
+        engine.execute_client_cmd("buy hegrenade")
+    end
+
+    if ui.get_bool("buy_molly") then
+        engine.execute_client_cmd("buy molotov; buy incgrenade")
+    end
+
+    if ui.get_bool("buy_smoke") then
+        engine.execute_client_cmd("buy smokegrenade")
+    end
+    
+    if ui.get_bool("buy_armor") then
+        engine.execute_client_cmd("buy vesthelm, buy vest")
+    end
+
+    if ui.get_bool("buy_taser") then
+        engine.execute_client_cmd("buy taser")
+    end
+
+    if ui.get_bool("buy_defuse") then
+        engine.execute_client_cmd("buy defuser")
+    end
+end
 end
 
 client.register_callback("fire_game_event", buy_bot)
